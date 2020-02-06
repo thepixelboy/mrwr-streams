bcf339dbce3bfb2bccdcb114713b79df93c17aa0
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-02-06 16:06:15 +0100

    Handling Forms with Redux form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,10 +15,10 @@ const App = () => {
         <div>
           <Header />
           <Route path='/' exact component={StreamList} />
-          <Route path='/stream/new' exact component={StreamCreate} />
-          <Route path='/stream/edit' exact component={StreamEdit} />
-          <Route path='/stream/delete' exact component={StreamDelete} />
-          <Route path='/stream/show' exact component={StreamShow} />
+          <Route path='/streams/new' exact component={StreamCreate} />
+          <Route path='/streams/edit' exact component={StreamEdit} />
+          <Route path='/streams/delete' exact component={StreamDelete} />
+          <Route path='/streams/show' exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
