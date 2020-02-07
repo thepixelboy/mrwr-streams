1e5a7f6696554b56b2e07d30c9279ef28b6b5134
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-02-07 12:36:28 +0100

    Implementing programmatic navigation

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams';
+import history from '../history';
 
 import {
   SIGN_IN,
@@ -23,10 +24,12 @@ export const signOut = () => {
   };
 };
 
-export const createStream = formValues => async dispatch => {
-  const response = await streams.post('/streams', formValues);
+export const createStream = formValues => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post('/streams', { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  history.push('/');
 };
 
 export const fetchStreams = () => async dispatch => {
